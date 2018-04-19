5992a9fb4801e76cee43eec605a0c349bd5348e0
zhuiyi <aiscly@gmail.com>
2018-04-19 16:12:49 +0800

    first-commit

---
+++ my_blog/article/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 50, blank = True)
+    date_time = models.DateTimeField(auto_now_add = True)
+    content = models.TextField(blank = True, null = True)
+
+    def __uncode__(self):
+	return self.title
+
+    class Meta:
+	ordering = ['-date_time']
